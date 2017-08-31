52739f8abf7a2c72d562bab4ce2a85e9984eda71
Maximilian Schwarzmüller <business@academind.com>
2017-08-31 09:13:30 +0200

    added sw!

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,6 +2,8 @@ var title = document.querySelector('.title');
 var courseFeatureElements = document.querySelectorAll('.course-feature');
 var button = document.querySelector('button');
 
+navigator.serviceWorker.register('/sw.js');
+
 function animate() {
   title.classList.remove('animate-in');
   for (var i = 0; i < courseFeatureElements.length; i++) {
